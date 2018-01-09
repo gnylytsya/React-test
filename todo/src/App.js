cf6881af97c5cab69aee0409341dc63b14096549
gnylytsya <gnylytsya@gmail.com>
2018-01-09 19:45:53 +0200

    add try-react

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,21 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react'
+import {
+    Button,
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    Grid,
+    Row,
+    Col,
+    ListGroup,
+    ListGroupItem
+} from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    componentWillMount(){
+
+        this.onAddInputHandleChamge = this.onAddInputHandleChamge.bind(this)
+
+        this.state = {
+            tasks: [],
+            addInputValue: ''
+        }
+    }
+
+    onAddInputHandleChamge(e){
+        this.setState({
+            addInputValue: e.target.value
+        })
+    }
+
+    onAddClick() {
+        let { tasks, addInputValue } = this.state;
+
+        tasks.push( {
+            title: addInputValue,
+            isDone: false
+
+        })
+        this.setState({
+            tasks
+        })
+    }
+
+    render () {
+        const { addInputValue } = this.state;
+        return (
+            <Grid>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
+                <Row className="show-grid">
+                    <Col xs={12} md={8}>
+                        <form>
+                            <FormGroup
+                                controlId="addNewTask"
+                            >
+                                <ControlLabel>Working example with validation</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    value={addInputValue}
+                                    placeholder="Enter the task"
+                                    onChange={this.onAddInputHandleChamge}
+                                />
+                                <FormControl.Feedback />
+                            </FormGroup>
+                        </form>
+                    </Col>
+                    <Col xs={6} md={4}>
+                        asd
+                    </Col>
+                </Row>
+                <Row className="show-grid">
+                    <Col xs={12} md={8}>
+                        <Button bsStyle="primary">Primary</Button>
+                    </Col>
+                    <Col xs={6} md={4}>asd</Col>
+                </Row>
+
+
+            </Grid>
+        )
+    }
 }
 
-export default App;
+
+export default App
